e33aa0147d55589e2882cde3c48fac64be85afb5
chan0kan <166641395+chan0kan@users.noreply.github.com>
2024-05-22 16:06:15 -0300

    Update CharacterBody2D.gd
    cambie los nombres de las animaciones y de las teclas a las mias

--- Scripts/CharacterBody2D.gd
+++ Scripts/CharacterBody2D.gd
@@ -1,18 +1,66 @@
 extends CharacterBody2D
 
-const SALTO =  -300
-const VELOCIDAD = 400
+var SPEED = 300.0
+const JUMP_VELOCITY = -2100
+const GRAVITY = 98.0
 
-var GRAVITY = 30
+@onready var anim = $AnimatedSprite2D
+@onready var timer = $Timer
 
+var attacking = false  # Variable para rastrear si el personaje está atacando
+var move : bool = false  # Varible para determinar si el personaje se esta moviendo
 
+func _ready():
+	pass
 
+# De aca 
 func _process(delta):
-	
-	var GRAVITY = velocity + GRAVITY
-	
-	if Input.is_key_pressed(KEY_W):
-		
-		
+	if not is_on_floor():
+		velocity.y += GRAVITY
+		SPEED = 670
+	else:
+		SPEED = 300
+		velocity.y = 0
+# Hasta acá sabes lo que hace (define las propiedades del salto)
+
+	# Manejar el ataque
+	if Input.is_action_just_pressed("atack") and not attacking:
+		anim.play("atack")  # Aca tenia un ("attack", false) para que no se repdoduzca en bucle pero lo arregle en el nodo (y a mi me lo rompe)
+		attacking = true  # Establece el estado de ataque como verdadero
+
+	if not attacking:
+
+		anim.play("quiet")
+
+	# Manejar el salto
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# Manejar el movimiento horizontal
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if Input.is_action_pressed("ui_focus_prev") and direction > 0:
+			SPEED = 600
+
+		if direction != 0:
+			velocity.x = direction * SPEED
+
+			if direction < 0:
+				anim.play("walk(atras)")
+			else:
+				anim.play("walk(adelante)")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			anim.play("quiet")
+
+	move_and_slide()
 
 
+func _on_animated_sprite_2d_animation_finished():
+	attacking = false
+
+func _on_timer_timeout():
+
+	attacking = false
+	print("atack false")
+
+	timer.stop()
